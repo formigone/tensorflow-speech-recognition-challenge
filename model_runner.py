af971843bfdd06e92b5033a125f924542c687f67
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-11-27 21:21:11 -0700

    Add sample dataset to verify wiring of model_fn

--- model_runner.py
+++ model_runner.py
@@ -1,13 +1,14 @@
 import argparse
 import sys
 
+import numpy as np
 import tensorflow as tf
 from models import simple_cnn
 from util.data import gen_input_fn_csv
 
 FLAGS = None
-LEARNING_RATE = 0.001
-tf.logging.set_verbosity(tf.logging.INFO)
+LEARNING_RATE = 1E-6
+tf.logging.set_verbosity(tf.logging.DEBUG)
 
 
 def main(args):
@@ -18,19 +19,25 @@ def main(args):
     model = tf.estimator.Estimator(model_dir=FLAGS.model_dir, model_fn=simple_cnn.model_fn, params=model_params)
 
     if FLAGS.mode == 'train':
-        print('--------')
-        print('Training')
-        print('--------')
+        for i in range(100):
+            print('--------')
+            print('Training')
+            print('--------')
+            model.train(input_fn=gen_input_fn_csv(FLAGS.input_file, num_epochs=500, target_dtype=np.float32))
 
-        model.train(input_fn=gen_input_fn_csv(FLAGS.input_file, num_epochs=5))
+            print('--------')
+            print('Evaluating')
+            print('--------')
+            evaluation = model.evaluate(input_fn=gen_input_fn_csv(FLAGS.input_file, num_epochs=1, target_dtype=np.float32))
+            print(evaluation)
     elif FLAGS.mode == 'validate':
         print('----------')
         print('Validating')
         print('----------')
 
-        predictions = model.predict(input_fn=gen_input_fn_csv(FLAGS.input_file))
+        predictions = model.predict(input_fn=gen_input_fn_csv(FLAGS.input_file, num_epochs=1, target_dtype=np.float32))
         for i, p in enumerate(predictions):
-            print("Prediction %s: %s" % (i + 1, p["ages"]))
+            print("Prediction %s: %s" % (i + 1, p["predictions"]))
     else:
         raise ValueError('Invalid mode')
 
@@ -39,13 +46,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, metavar='', required=True, help='Must be [train|validate]')
     parser.add_argument('--input_file', type=str, metavar='', required=True, help='Path to input data CSV file')
-    parser.add_argument('--testing_input_file', type=str, metavar='', default=None, help='Path to testing input data CSV file')
     parser.add_argument('--model_dir', type=str, metavar='', default=None, help='Path to save the model to')
 
     FLAGS, unparsed = parser.parse_known_args()
-
-    if FLAGS.mode == 'train' and FLAGS.testing_input_file is None:
-        print('Missing argument --testing_input_file. This file must be provided when --mode == train')
-        exit()
 
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- models/simple_cnn.py
+++ models/simple_cnn.py
@@ -2,15 +2,29 @@ import tensorflow as tf
 
 
 def model_fn(features, labels, mode, params):
-    print('labels:')
-    print(labels)
-    l1 = tf.layers.dense(features['x'], 15939, activation=tf.nn.relu, name='dense_1')
-    output_layer = tf.layers.dense(l1, 1, name='output_layer')
-    predictions = tf.reshape(output_layer, [-1])
+    print('-- model_fn --')
+    print('params: {0}'.format(params))
+
+    l1 = tf.layers.dense(features['x'], 1000, activation=tf.nn.relu, name='dense_1')
+    l2 = tf.layers.dense(l1, 1000, activation=tf.nn.relu, name='dense_2')
+    l3 = tf.layers.dense(l2, 1000, activation=tf.nn.relu, name='dense_3')
+    output_layer = tf.layers.dense(l3, 1, name='output_layer')
+
+    predictions = tf.reshape(output_layer, [-1], name='predictions')
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions={'pred': predictions})
+        return tf.estimator.EstimatorSpec(mode=mode, predictions={'predictions': predictions})
+
+    loss = tf.losses.mean_squared_error(labels, predictions)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
+    train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+    eval_metric_ops = {
+        'rmse': tf.metrics.root_mean_squared_error(tf.cast(labels, tf.float32), predictions)
+    }
 
     return tf.estimator.EstimatorSpec(
         mode=mode,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=eval_metric_ops
     )
