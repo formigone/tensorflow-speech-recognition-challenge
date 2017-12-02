ea65fa794dceba29860d05f4128b969c40a894fb
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-12-01 21:43:33 -0700

    Split input lists into multiple csv files

--- sound_to_img.py
+++ sound_to_img.py
@@ -6,10 +6,11 @@ from scipy.io import wavfile
 from util.labels import label2int, int2label
 
 
-def parse_data_file(file_path, offset, max):
+def parse_data_file(file_path, offset, max, shape=(99, 161)):
     m = 0
     i = 0
     x_len = 0
+    data = []
     file = open(file_path, 'r')
     for line in file:
         if i < offset:
@@ -21,33 +22,18 @@ def parse_data_file(file_path, offset, max):
         path = 'data_speech_commands_v0.01/' + key + '/' + wav
         sr, sound = wavfile.read(path)
         spec = log_specgram(sound, sr)
+        if spec.shape != shape:
+            continue
         flat = spec.reshape((1, spec.shape[0] * spec.shape[1]))
         if x_len == 0:
             x_len = flat.shape[1]
+        flat = np.char.mod('%f', flat[0])
+        data.append(','.join(flat) + ',' + str(label2int(key)))
         m += 1
     file.close()
 
-    print(str(m) + ',' + str(x_len))
-
-    i = 0
-    file = open(file_path, 'r')
-    for line in file:
-        if i < offset:
-            i += 1
-            continue
-        if i >= offset + max:
-            break
-        i += 1
-        key, wav = line.split()
-        path = 'data_speech_commands_v0.01/' + key + '/' + wav
-        sr, sound = wavfile.read(path)
-        spec = log_specgram(sound, sr)
-        flat = spec.reshape((1, spec.shape[0] * spec.shape[1]))
-        # print(','.join(np.char.mod('%f', flat[0])) + ',' + str(label2int(key)))
-        flat = np.char.mod('%f', flat[0])
-        if len(flat) == x_len:
-            print(','.join(flat) + ',' + str(label2int(key)))
-    file.close()
+    header = str(m) + ',' + str(x_len)
+    return header, data
 
 
 def log_specgram(audio, sample_rate, window_size=20,
@@ -92,8 +78,8 @@ def graph_specgrams(filename, save_dir, w, h):
 # parse_data_file('training-set.text')
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_list', type=str, metavar='', default=None, help='Path to raw list of files and labels')
-parser.add_argument('--offset', type=int, metavar='', default=0, help='Offset to parse inputs from')
-parser.add_argument('--max', type=int, metavar='', default=5000, help='Max inputs to parse')
+parser.add_argument('--batch_size', type=int, metavar='', default=5000, help='Max inputs to parse per file')
+parser.add_argument('--filename', type=str, metavar='', default=None, help='Name of output files')
 parser.add_argument('--debug', type=int, metavar='', default=0, help='Either 0 or 1')
 parser.add_argument('--graph', type=int, metavar='', default=0, help='Either 0 or 1')
 parser.add_argument('--graph_save_dir', type=str, metavar='', default='', help='Either 0 or 1')
@@ -104,4 +90,18 @@ if FLAGS.debug:
 elif FLAGS.graph:
     graph_specgrams(FLAGS.input_list, FLAGS.graph_save_dir, 99, 161)
 else:
-    parse_data_file(FLAGS.input_list, FLAGS.offset, FLAGS.max)
+    offset = 0
+    i = 1
+    while True:
+        header, data = parse_data_file(FLAGS.input_list, offset, FLAGS.batch_size)
+        data_len = len(data)
+        offset += data_len
+        filename = FLAGS.filename + str(i) + '.csv'
+        fh = open(filename, 'w')
+        fh.write(header + '\n')
+        fh.write('\n'.join(data) + '\n')
+        fh.close()
+        print('Created {} - {} lines'.format(filename, len(data)))
+        i += 1
+        if data_len < FLAGS.batch_size:
+            break
